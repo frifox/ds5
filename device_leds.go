b77c7e878f6b23a7f9ee1ca5a337008e50af520f
Denis G <denis@trendcapital.com>
2021-10-11 15:04:52 -0700

    de-gofmt

--- device_leds.go
+++ device_leds.go
@@ -10,12 +10,12 @@ func (l *PlayerLEDs) AllOff() {
 
 //  mimic PS5 behavior
 var ledPlayerMap = map[uint8][]uint8{
-	0: {},
-	1: {2},
-	2: {1, 3},
-	3: {0, 2, 4},
-	4: {0, 1, 3, 4},
-	5: {0, 1, 2, 3, 4},
+	0: {},              // _____
+	1: {2},             // __X__
+	2: {1, 3},          // _X_X_
+	3: {0, 2, 4},       // X_X_X
+	4: {0, 1, 3, 4},    // XX_XX
+	5: {0, 1, 2, 3, 4}, // XXXXX
 }
 
 func (l *PlayerLEDs) SetPlayer(playerID uint8) {
@@ -29,12 +29,12 @@ func (l *PlayerLEDs) SetPlayer(playerID uint8) {
 
 // volume bar
 var ledBarMap = map[uint8][]uint8{
-	0: {},
-	1: {0},
-	2: {0, 1},
-	3: {0, 1, 2},
-	4: {0, 1, 2, 3},
-	5: {0, 1, 2, 3, 4},
+	0: {},              // _____
+	1: {0},             // X____
+	2: {0, 1},          // XX___
+	3: {0, 1, 2},       // XXX__
+	4: {0, 1, 2, 3},    // XXXX_
+	5: {0, 1, 2, 3, 4}, // XXXXX
 }
 
 func (l *PlayerLEDs) SetBar(count uint8) {
